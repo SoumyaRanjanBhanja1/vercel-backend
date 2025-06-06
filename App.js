c16579cc66592cff89e63979d11e78a1553cfa55
SoumyaRanjanBhanja1 <soumyabhanja180@gmail.com>
2025-06-06 21:21:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
